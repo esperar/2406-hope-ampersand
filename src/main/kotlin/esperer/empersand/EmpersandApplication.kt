98c5b4428e6eae068f1ad851979d5fcbcbd0d2fd
aesperer <piyrw9754@gmail.com>
2023-04-26 11:48:54 +0900

    :tada: begint

---
+++ src/main/kotlin/esperer/empersand/EmpersandApplication.kt
@@ -0,0 +1,11 @@
+package esperer.empersand
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmpersandApplication
+
+fun main(args: Array<String>) {
+	runApplication<EmpersandApplication>(*args)
+}
---
+++ src/test/kotlin/esperer/empersand/EmpersandApplicationTests.kt
@@ -0,0 +1,13 @@
+package esperer.empersand
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmpersandApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
