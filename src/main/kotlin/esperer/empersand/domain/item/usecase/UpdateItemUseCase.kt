7bfa9565ed15a9ce8592b3692e5794323d9ae10d
aesperer <piyrw9754@gmail.com>
2023-04-27 09:29:39 +0900

    이거 왜있노
    '

--- src/main/kotlin/esperer/empersand/domain/item/usecase/UpdateItemUseCase.kt
+++ src/main/kotlin/esperer/empersand/domain/item/usecase/UpdateItemUseCase.kt
@@ -25,17 +25,3 @@ class UpdateItemUseCase(
             ) }
     }
 }
-
-@UseCase
-class UpdateItemUseCase(
-    private val commandItemPort: CommandItemPort,
-    private val userSecurityPort: UserSecurityPort,
-    private val userPort: UserPort,
-    private val userMapper: UserMapper
-) {
-
-    fun execute(id: UUID, request: UpdateItemRequest){
-        val user = userPort.queryUserByEmail(userSecurityPort.queryCurrentUserEmail())
-
-    }
-}
